453ebe0a8ff9399b96a508f121f660de0a0f3554
Abhishek Rawat <131130116+Abhirawat623@users.noreply.github.com>
2024-05-20 19:33:54 +0530

    deployment

---
+++ src/components/Cards.jsx
@@ -0,0 +1,13 @@
+export const Cards=()=>{
+
+    return(
+        <div className="flex flex-col bg-purple-400 w-44 h-60 items-center rounded-xl p-2">
+            <div className="flex flex-row gap-x-6 ">
+                <span className="bg-black text-white rounded-sm text-center">2024</span>
+                By Pablo </div>
+                <span className="text-center">Collectible Name</span>
+                <img src="./artist.jpg" className="w-4/5 h-3/5 border-black border-[2px] rounded-xl "/>
+
+        </div>
+    )
+}
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,57 @@
+import {Cards} from "./Cards"
 export const Main =()=>{
 
     return(
         <div className="bg-sec h-screen">
-            <div className="relative bg-sec text-yellow-500 top-5 left-2 flex flex-row items-center gap-x-2 pl-6 pt-10"> 
+            {/* <div className="relative bg-sec text-yellow-500 top-5 left-2 flex flex-row items-center gap-x-2 pl-6 pt-10"> 
 <img  className="h-9 w-10 bg-yellow-500"  src='./logo.png' alt='logo'/>
 <h1 className="text-5xl font-semibold">Astrix.</h1>
+</div> */}
+
+{/* middle section */}
+<div className="flex flex-row justify-center items-between  relative bg-sec">
+    <div>
+        
+    <div className="flex flex-col  items-start ">
+        {/* para */}
+        <div className="flex flex-col justify-start text-white text-4xl font-semibold"><p>Exlpore Your First</p>
+            <p>Collectible</p></div>
+        {/* subheading */}
+        <div className="flex flex-col justify-start  text-white text-6xl font-bold">
+            <p>Meta</p>
+            <p>Lives</p></div>
+    </div>
+    </div>
+    <div className="bg-purple-400 w-full h-[3.2rem] relative rotate-head">
+<p className="animate-para text-3xl">Lorem,re voluptareraepe ex. Modi nostribus eius.</p></div>
+    <div className="flex flex-col justify-start right-[12rem] absolute gap-y-12">
+        {/* para */}
+        <div className="flex flex-col justify-start text-white text-2xl font-semibold"><p>Exlpore Your First</p>
+            <p>Collectible</p></div>
+        {/* subheading */}
+        <div className="flex flex-col justify-start text-white text-6xl font-bold">
+            <p>Meta</p>
+            <p>Lives</p></div>
+            {/* details */}
+            <div className="flex flex-col justify-start text-white text-2xl font-semibold">
+            <p>Live in Astrix</p>
+            <p>Lorem i Volupt odie hsdas</p>
+            <p>Lorem i Voluptas3d</p>
+            <p>Lorem i Volu3dptas</p>
+            <p>Lorem i Vold3dduptas</p>
+           </div>
+           <div className="flex flex-row justify-start items-center text-white text-2xl font-semibold">
+            <img className=" h-16 w-28" src="./cascade.png" alt="cascade"/>
+            <p>22K People Interested</p></div>
+            <div className="flex flex-row justify-start items-center text-white gap-x-5">
+            <Cards/>
+            <Cards/>
+            <Cards/>
+            </div>
+            <button className="rounded-xl bg-yellow-300 w-1/2 text-3xl mb-2 ">Join Waitlist</button>
+    </div>
 </div>
 
-<div className="pl-96 bg-purple-400 max-h-[4.5rem] rotate-head ">Lorem,re voluptarerete maiores saepe ex. Modi nostrum ea recusandae voluptatibus eius.
-Aniti nemo ?</div>
-
-        </div>
+</div>
     )
 }
